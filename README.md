1da6e0d806598f4969e52b9b9b6cdffd3c65ec7c
arisuchan <castromario456@gmail.com>
2019-02-10 18:57:29 -0200

    Primeiro commit do site ecchi

---
+++ README.md
@@ -0,0 +1,5 @@
+# Projetinho de blog feito com html e css
+
+#### Esse projeto foi implementado com a finalidade de treinar as minhas skills em HTML e CSS.
+
+
---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>EcchiBlog</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<div id="navegacao">
+<div id="area">
+	<h1 id="logo"><span class="verde">Ecchi</span>Blog</h1>
+	<div id="menu">
+		<a href="index.html">Home </a>
+		<a href="eroge.html">Eroge</a>
+		<a href="development.html">Development</a>
+		<a href="anime.html">Anime</a>
+	</div>
+</div>
+</div>
+
+<div id="area-principal">
+	<div id="postagem">
+		<h2>Titulo da postagem</h2>
+		<span class="data-postagem">Postado em 12 de março de 2015</span>
+		<img class="img-postagem" src="img/imagem1.jpg" width="300px" height="250px">
+		<p>Em japonês, hentai (変態 ou へんたい?) significa originalmente 'atitude estranha', 'comportamento fora do comum'. A palavra também é usada como uma abreviação da expressão hentai seiyoku (変態性欲?), que significa perversão sexual, e, quando usada coloquialmente, expressa um ato sexual em condições extremas. No Japão, o termo também é empregado com referência a mangás e animês que tenham forte conteúdo sexual.
+
+		Atualmente há duas definições para hentai: a primeira é a original japonesa; a segunda é a definição dada à palavra, em inglês.
+
+No Japão, o termo hentai se aplica a um material de conteúdo sexual bizarro, extremo ou mesmo criminoso, como, por exemplo, um estupro. Nos Estados Unidos, o termo é aplicado a qualquer animação, mangá ou cartoon com algum tipo de conteúdo sexual, seja erótico ou pornográfico, leve ou extremo. O conceito assumido erroneamente em inglês passou a definir um gênero que inclui todos os tipos de ilustrações com algum conteúdo sexual e se popularizou mais que o conceito original japonês. Enquanto em japonês tanto 'H' (ecchi) como ero (erótico) podem ser designar mangás ou animes de conteúdo sexual, hentai somente se aplica quando há situações inusitadas ou de perversão sexual ou mesmo ilícitas. Em síntese: no contexto japonês, quando se fala em hentai, trata-se de uma subdivisão de uma categoria muito mais ampla de mangá ero, enquanto que, em inglês, hentai designa o gênero de mangá erótico como um todo.[1]
+
+Já o sentido em japonês, que seria o correto, apenas se refere a um material sexual de conteúdo bizarro, extremo, "perverso" ou "anormal". Vale lembrar que também para os japoneses, hentai, é uma subcategoria de publicações ero (eróticas). Este tipo de conteúdo no Japão é classificado como 18-kin (18禁?) que significa proibido para menores de 18 anos, ou, Seijin Manga (成人漫画?) que seria mangá adulto. 
+		</p>
+		<span class="postado">Postado por Luca</span>
+	</div>
+
+	<div id="postagem">
+		<h2>Titulo da postagem 2</h2>
+		<span class="data-postagem">Postado em 12 de março de 2015</span>
+		<img class="img-postagem" src="img/imagem2.jpg" width="300px" height="250px">
+		<p>Em japonês, hentai (変態 ou へんたい?) significa originalmente 'atitude estranha', 'comportamento fora do comum'. A palavra também é usada como uma abreviação da expressão hentai seiyoku (変態性欲?), que significa perversão sexual, e, quando usada coloquialmente, expressa um ato sexual em condições extremas. No Japão, o termo também é empregado com referência a mangás e animês que tenham forte conteúdo sexual.
+
+		Atualmente há duas definições para hentai: a primeira é a original japonesa; a segunda é a definição dada à palavra, em inglês.
+
+No Japão, o termo hentai se aplica a um material de conteúdo sexual bizarro, extremo ou mesmo criminoso, como, por exemplo, um estupro. Nos Estados Unidos, o termo é aplicado a qualquer animação, mangá ou cartoon com algum tipo de conteúdo sexual, seja erótico ou pornográfico, leve ou extremo. O conceito assumido erroneamente em inglês passou a definir um gênero que inclui todos os tipos de ilustrações com algum conteúdo sexual e se popularizou mais que o conceito original japonês. Enquanto em japonês tanto 'H' (ecchi) como ero (erótico) podem ser designar mangás ou animes de conteúdo sexual, hentai somente se aplica quando há situações inusitadas ou de perversão sexual ou mesmo ilícitas. Em síntese: no contexto japonês, quando se fala em hentai, trata-se de uma subdivisão de uma categoria muito mais ampla de mangá ero, enquanto que, em inglês, hentai designa o gênero de mangá erótico como um todo.[1]
+
+Já o sentido em japonês, que seria o correto, apenas se refere a um material sexual de conteúdo bizarro, extremo, "perverso" ou "anormal". Vale lembrar que também para os japoneses, hentai, é uma subcategoria de publicações ero (eróticas). Este tipo de conteúdo no Japão é classificado como 18-kin (18禁?) que significa proibido para menores de 18 anos, ou, Seijin Manga (成人漫画?) que seria mangá adulto. 
+		</p>
+		<span class="postado">Postado por Luca</span>
+	</div>
+
+</div>
+
+</body>
+</html>
